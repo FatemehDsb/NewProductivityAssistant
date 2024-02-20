beafa5eecdb349e29a14cbbf15b5b2777141423e
sirisab <143996815+sirisab@users.noreply.github.com>
2024-02-20 12:58:54 +0100

    adds todo functionality and html file

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
-let usernameInput = document.querySelector("#userName");
-let passwordInput = document.querySelector("#password");
-let registerBtn = document.querySelector("#registerBtn");
-let loginBtn = document.querySelector("#loginBtn");
+window.addEventListener("DOMContentLoaded", (event) => {
+  let usernameInput = document.getElementById("userName");
+  let passwordInput = document.getElementById("password");
 
-let registeredUsers= JSON.parse(localStorage.getItem("users")) || [];
+  let todoInput = document.getElementById("todoInput");
+  let todoUl = document.getElementById("todoUl");
 
-registerBtn.addEventListener("click", ()=>{
-    let newUsername= usernameInput.value;
-    let newPassword = passwordInput.value;
+  let registeredUsers =
+    JSON.parse(localStorage.getItem("registeredUsers")) || [];
 
-    let newUser={
+  let registerBtn = document.getElementById("registerBtn");
+
+  if (registerBtn) {
+    registerBtn.addEventListener("click", () => {
+      let newUsername = usernameInput.value;
+      let newPassword = passwordInput.value;
+
+      let newUser = {
         newUsername,
-        newPassword
-    }
-   
-    registeredUsers.push(newUser); 
+        newPassword,
+      };
 
-    localStorage.setItem("users", JSON.stringify(registeredUsers));
+      registeredUsers.push(newUser);
+
+      localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
+    });
+  }
+  //localStorage.clear();
+
+  let loginBtn = document.getElementById("loginBtn");
+  if (loginBtn) {
+    loginBtn.addEventListener("click", () => {
+      let userName = usernameInput.value;
+      let password = passwordInput.value;
+
+      let user = registeredUsers.find(
+        (user) => user.newUsername === userName && user.newPassword === password
+      );
+
+      if (user) {
+        window.location.assign("todo.html"), console.log("success");
+      } else {
+        console.log("fail");
+      }
+    });
+  }
+
+  let addTodoBtn = document.getElementById("addTodoBtn");
+  if (addTodoBtn) {
+    addTodoBtn.addEventListener("click", () => {
+      let todoInputValue = todoInput.value;
+      let todoLi = document.createElement("li");
+      todoLi.innerHTML = todoInputValue;
+      todoUl.append(todoLi);
+    });
+  }
 });
-
-//localStorage.clear();
-
-loginBtn.addEventListener("click", () => {
-    let userName = usernameInput.value;
-    let password = passwordInput.value;
-
-    let user = registeredUsers.find(
-        (user) => user.newUsername === userName && user.newPassword === password 
-    );
-
-    if(user){
-        console.log('success');
-    } else {
-        console.log('fail');
-    }
-
-})
-
-
